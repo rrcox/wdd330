87c9f56e89b834a5c8cc9632cc43bed8243daf3f
Rob Cox <rob_cox@binary-development.com>
2022-10-16 23:36:00 -0400

    Modules test

--- main.js
+++ main.js
@@ -1,3 +1,147 @@
+// const hikeList = [
+//     {
+//       name: "Bechler Falls",
+//       imgSrc: "falls.jpg",
+//       imgAlt: "Image of Bechler Falls",
+//       distance: "3 miles",
+//       difficulty: "Easy",
+//       description:
+//         "Beautiful short hike along the Bechler river to Bechler Falls",
+//       directions:
+//         "Take Highway 20 north to Ashton. Turn right into the town and continue \
+//         through. Follow that road for a few miles then turn left again onto the \
+//         Cave Falls road.Drive to the end of the Cave Falls road. There is a \
+//         parking area at the trailhead."
+//     },
+//     {
+//       name: "Teton Canyon",
+//       imgSrc: "falls.jpg",
+//       imgAlt: "Image of Bechler Falls",
+//       distance: "3 miles",
+//       difficulty: "Easy",
+//       description: "Beautiful short (or long) hike through Teton Canyon.",
+//       directions:
+//         "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. \
+//         Follow that road for a few miles then turn right onto Stateline \
+//         Road for a short distance, then left onto Alta Road. Veer right after \
+//         Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+//     },
+//     {
+//       name: "Denanda Falls",
+//       imgSrc: "falls.jpg",
+//       imgAlt: "Image of Bechler Falls",
+//       distance: "7 miles",
+//       difficulty: "Moderate",
+//       description:
+//         "Beautiful hike through Bechler meadows river to Denanda Falls",
+//       directions:
+//         "Take Highway 20 north to Ashton. Turn right into the town and continue \
+//         through. Follow that road for a few miles then turn left again onto the \
+//         Cave Falls road. Drive to until you see the sign for Bechler Meadows on \
+//         the left. Turn there. There is a parking area at the trailhead."
+//     }
+// ];
+  
+// const imgBasePath = "./images/";
+
+// // export default class Hikes {
+// class Hikes {
+//     constructor(elementId) {
+//         this.parentElement = document.getElementById(elementId);
+//     }
+
+//     getAllHikes() {
+//         return hikeList;
+//     }
+
+//     getHikeByName(hikeName) {
+//         return this.getAllHikes().find(hike => hike.name === hikeName);
+//     }
+    
+//     showHikeList() {       
+//         renderHikeList(this.parentElement, hikeList);
+//     }
+
+//     showOneHike(hikeName) {
+//         const hike = this.getHikeByName(hikeName);
+//         const element = renderOneHikeFull(hike);
+//         this.parentElement.replaceChildren(element);
+//         const backButton = this.buildBackButton();
+//         this.parentElement.appendChild(backButton);
+//     }
+    
+//     addHikeListener() {
+//         const children = Array.from(this.parentElement.children);
+//         children.forEach( (child) => {
+//             child.addEventListener("click", (e) => {
+//                 const hikeName = e.target.closest("li").children[0].innerText;
+//                 this.showOneHike(hikeName);
+//             })
+//         });
+//     }
+
+//     buildBackButton() {
+//         const backButton = document.createElement("button");
+//         backButton.innerText = "Back to Great Hikes List";
+//         backButton.addEventListener("click", (e) => {
+//             this.parentElement.replaceChildren();
+//             renderHikeList(this.parentElement, hikeList);
+//             this.addHikeListener();
+//         });
+
+//        return backButton;
+//     }
+// }
+
+// //*****************************************************************************
+// // Auxillary Module-Scope Functions
+// //*****************************************************************************
+
+// function renderHikeList(parent, hikes) {
+//     hikeList.forEach(hike => {
+//         const node = renderOneHikeLight(hike);
+//         parent.appendChild(node);
+//     })
+// }
+
+// function renderOneHikeLight(hike) {
+//     const item = document.createElement("li");
+//     item.innerHTML = ` <h2>${hike.name}</h2>
+//     <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+//     <div>
+//             <div>
+//                 <h3>Distance</h3>
+//                 <p>${hike.distance}</p>
+//             </div>
+//             <div>
+//                 <h3>Difficulty</h3>
+//                 <p>${hike.difficulty}</p>
+//             </div>                
+//     </div>`;
+//     return item;
+// }
+
+// function renderOneHikeFull(hike) {
+//     const item = document.createElement("li");
+//     item.innerHTML = ` <h2>${hike.name}</h2>
+//     <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+//     <div>
+//             <div>
+//                 <h3>Distance</h3>
+//                 <p>${hike.distance}</p>
+//             </div>
+//             <div>
+//                 <h3>Difficulty</h3>
+//                 <p>${hike.difficulty}</p>
+//             </div>
+//             <div> and other stuff </div>            
+//     </div>`;
+//     return item;
+// }
+
+//-------main.js----------------------  
 import Hikes from './modules/hikes.js';
 
-console.log("test");
+const hikes = new Hikes("hikes");
+hikes.showHikeList();
+hikes.addHikeListener();
--- modules/hikes.js
+++ modules/hikes.js
@@ -40,53 +40,88 @@ const hikeList = [
         Cave Falls road. Drive to until you see the sign for Bechler Meadows on \
         the left. Turn there. There is a parking area at the trailhead."
     }
-  ];
+];
   
-  const imgBasePath = "./images/";
-  //on load grab the array and insert it into the page on load
-  
-  export default class Hikes {
+const imgBasePath = "./images/";
+
+export default class Hikes {
+// class Hikes {
     constructor(elementId) {
-      this.parentElement = document.getElementById(elementId);
-      // we need a back button to return back to the list. This will build it 
-      // and hide it. When we need it we just need to remove the 'hidden' class
-      this.backButton = this.buildBackButton();
+        this.parentElement = document.getElementById(elementId);
     }
-    // why is this function necessary?  hikeList is not exported, and so it cannot 
-    // be seen outside of this module. I added this in case I ever need the list of 
-    // hikes outside of the module. This also sets me up nicely if my data were to 
-    // move. I can just change this method to the new source and everything will 
-    // still work if I only access the data through this getter.
+
     getAllHikes() {
-      return hikeList;
+        return hikeList;
     }
-    // For the first stretch we will need to get just one hike.
+
     getHikeByName(hikeName) {
-      return this.getAllHikes().find(hike => hike.name === hikeName);
+        return this.getAllHikes().find(hike => hike.name === hikeName);
     }
-    //show a list of hikes in the parentElement
-    showHikeList() {}
-    // show one hike with full details in the parentElement
-    showOneHike(hikeName) {}
-    // in order to show the details of a hike ontouchend we will need to attach 
-    // a listener AFTER the list of hikes has been built. The function below does 
-    // that.
+    
+    showHikeList() {       
+        renderHikeList(this.parentElement, hikeList);
+    }
+
+    showOneHike(hikeName) {
+        const hike = this.getHikeByName(hikeName);
+        const element = renderOneHikeFull(hike);
+        this.parentElement.replaceChildren(element);
+        const backButton = this.buildBackButton();
+        this.parentElement.appendChild(backButton);
+    }
+    
     addHikeListener() {
-      // We need to loop through the children of our list and attach a listener 
-      // to each, remember though that children is a nodeList...not an array. 
-      // So in order to use something like a forEach we need to convert it to an array.
+        const children = Array.from(this.parentElement.children);
+        children.forEach( (child) => {
+            child.addEventListener("click", (e) => {
+                const hikeName = e.target.closest("li").children[0].innerText;
+                this.showOneHike(hikeName);
+            })
+        });
     }
+
     buildBackButton() {
-      const backButton = document.createElement("button");
-  
-      return backButton;
+        const backButton = document.createElement("button");
+        backButton.innerText = "Back to Great Hikes List";
+        backButton.addEventListener("click", (e) => {
+            this.parentElement.replaceChildren();
+            renderHikeList(this.parentElement, hikeList);
+            this.addHikeListener();
+        });
+
+       return backButton;
     }
-  }
-  // methods responsible for building HTML.  Why aren't these in the class?  
-  // They don't really need to be, and by moving them outside of the exported
-  // class, they cannot be called outside the module...they become private.
-  function renderHikeList(parent, hikes) {}
-  function renderOneHikeLight(hike) {
+}
+
+//*****************************************************************************
+// Auxillary Module-Scope Functions
+//*****************************************************************************
+
+function renderHikeList(parent, hikes) {
+    hikeList.forEach(hike => {
+        const node = renderOneHikeLight(hike);
+        parent.appendChild(node);
+    })
+}
+
+function renderOneHikeLight(hike) {
+    const item = document.createElement("li");
+    item.innerHTML = ` <h2>${hike.name}</h2>
+    <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+    <div>
+            <div>
+                <h3>Distance</h3>
+                <p>${hike.distance}</p>
+            </div>
+            <div>
+                <h3>Difficulty</h3>
+                <p>${hike.difficulty}</p>
+            </div>                
+    </div>`;
+    return item;
+}
+
+function renderOneHikeFull(hike) {
     const item = document.createElement("li");
     item.innerHTML = ` <h2>${hike.name}</h2>
     <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
@@ -99,17 +134,7 @@ const hikeList = [
                 <h3>Difficulty</h3>
                 <p>${hike.difficulty}</p>
             </div>
+            <div> and other stuff </div>            
     </div>`;
     return item;
-  }
-  function renderOneHikeFull(hike) {
-    const item = document.createElement("li");
-  
-    return item;
-  }
-  Footer
-  © 2022 GitHub, Inc.
-  Footer navigation
-  Terms
-  Privacy
-  Sec
+}
