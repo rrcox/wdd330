b0836448573beda15ba857d4fdfd0d919cec38f1
Rob Cox <rob_cox@binary-development.com>
2022-10-31 01:14:48 -0400

    Modularized Todos Fix 5

--- todo/js/main.js
+++ todo/js/main.js
@@ -1,59 +1,6 @@
-// import Tasks from './modules/tasks.js';
+import Tasks from './modules/tasks.js';
 // import Todos from './modules/todos.js';
 // import AddTaskForm from './modules/form.js';
-
-class Tasks {
-    constructor() {
-        this.list = this.getTaskList() || [];
-        this.filter = this.getTaskListFilter() || "all";
-    }
-
-    updateTask(node) {
-        let newList = this.list.map(task => {
-            if (task.id === node.id) {
-                if (task.complete === "true") {
-                    task.complete = "false"
-                } else {
-                    task.complete = "true";
-                }
-            } 
-            return task;
-        });
-        this.setTaskList(newList);
-    }
-
-    addToTaskList(taskName) {
-        const newTask = {
-            id: Date.now().toString(),
-            name: taskName,
-            complete: false.toString()
-        };
-        this.list.push(newTask);
-        return newTask;
-    }
-
-    removeFromTaskList(node) {
-        const id = node.children[0].children[1].id;
-        this.list = this.list.filter(task => task.id !== id);
-    }
-
-    getTaskList() {
-        return JSON.parse(window.localStorage.getItem('toDo'));
-    }
-
-    getTaskListFilter() {
-        return JSON.parse(window.localStorage.getItem('toDoFilter'));
-    }
-
-    setTaskList(list) {
-        window.localStorage.setItem('toDo', JSON.stringify(list));
-    }
-    
-    setTaskListFilter(filter) {
-        window.localStorage.setItem('toDoFilter', JSON.stringify(filter));
-        this.filter = filter;
-    }
-}
 
 class Todos {
     constructor(elementId, tasks) {
@@ -104,6 +51,12 @@ class Todos {
         this.show();
     }
     
+    #highlightFilterButton(filter, filterAllButton,  filterIncompleteButton, filterCompleteButton) {
+        filterAllButton.style.backgroundColor = (todos.getFilter() === "all" ? "green" : "white");
+        filterIncompleteButton.style.backgroundColor = (todos.getFilter() === "incomplete" ? "green" : "white");
+        filterCompleteButton.style.backgroundColor = (todos.getFilter() === "complete" ? "green" : "white");
+    }
+
     show() {       
         this.tasks.list.forEach(task => {
             const element = renderTask(task, this.#strikeThrough);
@@ -143,6 +96,36 @@ class Todos {
     setFilter(filter) {
         this.tasks.setTaskListFilter(filter);
     }
+
+    addFilterEventListeners() {
+        const filterAllButton = document.getElementById("filter-all-button");
+        const filterIncompleteButton = document.getElementById("filter-incomplete-button");
+        const filterCompleteButton = document.getElementById("filter-complete-button");
+    
+        this.#highlightFilterButton(this.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
+    
+        filterAllButton.addEventListener("click", (e) => {
+            this.hide();
+            this.setFilter("all");
+            this.show();
+            this.#highlightFilterButton(this.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
+        });
+        
+        filterIncompleteButton.addEventListener("click", (e) => {
+            this.hide();
+            this.setFilter("incomplete");
+            this.show();
+            this.#highlightFilterButton(this.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
+        });
+    
+        filterCompleteButton.addEventListener("click", (e) => {
+            this.hide();
+            this.setFilter("complete");
+            this.show();
+            this.#highlightFilterButton(this.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
+        });
+    
+    }
 }
 
 class AddTaskForm {
@@ -181,46 +164,9 @@ function renderTask(task, callback) {
     return element;
 }
 
-function highlightFilterButton(filter, filterAllButton,  filterIncompleteButton, filterCompleteButton) 
-{
-    filterAllButton.style.backgroundColor = (todos.getFilter() === "all" ? "green" : "white");
-    filterIncompleteButton.style.backgroundColor = (todos.getFilter() === "incomplete" ? "green" : "white");
-    filterCompleteButton.style.backgroundColor = (todos.getFilter() === "complete" ? "green" : "white");
-}
-
-function addFilterEventListeners(todos) {
-    const filterAllButton = document.getElementById("filter-all-button");
-    const filterIncompleteButton = document.getElementById("filter-incomplete-button");
-    const filterCompleteButton = document.getElementById("filter-complete-button");
-
-    highlightFilterButton(todos.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
-
-    filterAllButton.addEventListener("click", (e) => {
-        todos.hide();
-        todos.setFilter("all");
-        todos.show();
-        highlightFilterButton(todos.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
-    });
-    
-    filterIncompleteButton.addEventListener("click", (e) => {
-        todos.hide();
-        todos.setFilter("incomplete");
-        todos.show();
-        highlightFilterButton(todos.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
-    });
-
-    filterCompleteButton.addEventListener("click", (e) => {
-        todos.hide();
-        todos.setFilter("complete");
-        todos.show();
-        highlightFilterButton(todos.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
-    });
-
-}
-
 const tasks = new Tasks();
 const todos = new Todos("todo", tasks);
 const addTaskForm = new AddTaskForm("add-task-form", todos);
 
 todos.show();
-addFilterEventListeners(todos);
+todos.addFilterEventListeners();
