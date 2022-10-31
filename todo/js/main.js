3d751e143a27cfd4648a93518a259c8b5c673ca4
Rob Cox <rob_cox@binary-development.com>
2022-10-31 00:51:09 -0400

    Modularized Todos Fix 2

--- todo/js/main.js
+++ todo/js/main.js
@@ -1,6 +1,43 @@
 import Tasks from './modules/tasks.js';
-import {Todos, addFilterEventListeners} from './modules/todos.js';
+import Todos from './modules/todos.js';
 import AddTaskForm from './modules/form.js';
+
+function highlightFilterButton(filter, filterAllButton,  filterIncompleteButton, filterCompleteButton) 
+{
+    filterAllButton.style.backgroundColor = (todos.getFilter() === "all" ? "green" : "white");
+    filterIncompleteButton.style.backgroundColor = (todos.getFilter() === "incomplete" ? "green" : "white");
+    filterCompleteButton.style.backgroundColor = (todos.getFilter() === "complete" ? "green" : "white");
+}
+
+export function addFilterEventListeners(todos) {
+    const filterAllButton = document.getElementById("filter-all-button");
+    const filterIncompleteButton = document.getElementById("filter-incomplete-button");
+    const filterCompleteButton = document.getElementById("filter-complete-button");
+
+    highlightFilterButton(todos.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
+
+    filterAllButton.addEventListener("click", (e) => {
+        todos.hide();
+        todos.setFilter("all");
+        todos.show();
+        highlightFilterButton(todos.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
+    });
+    
+    filterIncompleteButton.addEventListener("click", (e) => {
+        todos.hide();
+        todos.setFilter("incomplete");
+        todos.show();
+        highlightFilterButton(todos.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
+    });
+
+    filterCompleteButton.addEventListener("click", (e) => {
+        todos.hide();
+        todos.setFilter("complete");
+        todos.show();
+        highlightFilterButton(todos.getFilter(), filterAllButton, filterIncompleteButton, filterCompleteButton);
+    });
+
+}
 
 const tasks = new Tasks();
 const todos = new Todos("todo", tasks);
